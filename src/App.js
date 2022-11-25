19dc9b490ec4f707b49a52e1d4615624aaf955be
Wail Abualela <wailabualela@gmail.com>
2022-11-25 21:23:49 +0200

    rm unwanted code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main className='contianer mt-5'>
+			<h1 className='text-primary text-center text-capitalize'>
+				<i className='bi bi-diagram-2-fill'></i>
+				Repos
+			</h1>
+		</main>
+	);
 }
 
 export default App;
